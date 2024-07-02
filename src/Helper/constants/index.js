5f447f1bf45c5c8246fdc551216efc62d59c5373
mahmud aslani <aslanimahmudtt@.com>
2024-07-02 14:07:07 +0330

    feat: add button component

---
+++ src/Helper/constants/index.js
@@ -0,0 +1,4 @@
+export const buttonMode = {
+  text: "button-text",
+  primary: "button-primary",
+};
---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { buttonMode } from "../../Helper/constants";
+import classNames from "classnames";
+
+function Button({ onClick, children, mode, className, ...rest }) {
+  const classes = classNames(
+    "button",
+    {
+      "btn-primary": mode === buttonMode.primary,
+      "btn-text": mode === buttonMode.text,
+      "btn-disable": mode === buttonMode.disable,
+    },
+    className
+  );
+  return (
+    <div id="button" onClick={onClick} className={classes} {...rest}>
+      {children}
+    </div>
+  );
+}
+Button.propTypes = {
+  children: PropTypes.any,
+  onClick: PropTypes.func,
+  className: PropTypes.string,
+  mode: PropTypes.oneOf(Object.keys(buttonMode).map((key) => buttonMode[key])),
+  rest: PropTypes.any,
+};
+
+export default Button;
